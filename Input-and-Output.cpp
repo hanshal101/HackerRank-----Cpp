79da08b93366d0a2f5847a7c457fda47c604b6d6
hanshal101 <82961842+hanshal101@users.noreply.github.com>
2023-01-13 21:17:47 +0530

    Create Input-and-Output.cpp

---
+++ Input-and-Output.cpp
@@ -0,0 +1,16 @@
+/*This problem explains us basic input and output functions in C++*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<a+b+c<<endl;   
+    return 0;
+}
